9c4ab7776bf09343c71df5235b3bc810512921f0
Bishwaranjan Satpathy <69967091+Bishwa1996@users.noreply.github.com>
2020-09-02 15:56:58 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string characterName = "Richie";
+    int characterAge;
+    characterAge = 25;
+    cout << "My name is " << characterName << endl;
+    cout << "I am "<< characterAge <<" years old" << endl;
+    cout << characterName <<" is a boy " << endl;
+    cout << "I have lived on earth for " << characterAge << " years" << endl;
+
+    return 0;
+}
