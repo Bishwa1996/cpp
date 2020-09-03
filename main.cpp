eca71c2a361c836297eca2c216d827254cc6cf5d
iamBishwa <bishwa1996@gmail.com>
2020-09-03 20:11:30 +0800

    2d array & nested loop

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 
 using namespace std;
-int power(int baseNum, int powerNum){
-int result = 1;
-for(int i = 0;i < powerNum;i++){
-    result = result* baseNum;
-}
-return result;
-}
+
+
 int main()
 {
-   cout<< power(2,3);
+    /*int numGrid[3][2]{ {1,2},
+                       {3,4},
+                       {5,6}
+    };
+    cout << numGrid[2][1];*/
+
+    int numGrid[3][2]{ {1,2},
+                       {3,4},
+                       {5,6}};
+    for (int i=0; i<3; i++){
+        for(int j=0; j<2; j++){
+            cout<< numGrid[i][j];
+        }
+        cout<<endl;
+    }
+
+
     return 0;
     }
 
