37772cba1bb2f851cb9c4144b2362f34a87de20a
iamBishwa <bishwa1996@gmail.com>
2020-09-03 21:43:25 +0800

    constructors

--- main.cpp
+++ main.cpp
@@ -7,16 +7,20 @@ public:
     string title;
     int pages;
     double price;
+    Book (string aTitle, int aPages, double aPrice){
+          title = aTitle;
+          pages = aPages;
+          price = aPrice;
+    }
 };
 
 int main()
 {
-Book book1;
-book1.title = "Wings of Fire";
-book1.pages = 345;
-book1.price = 110.50;
+Book book1("Wings of Fire",345,110.50);
+Book book2("Harry Potter",500,126.20);
 
-cout<< book1.price;
+
+cout<< book1.title;
     return 0;
     }
 
