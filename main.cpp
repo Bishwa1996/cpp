ce0dad045f9195cb76b036b29bb8b7ff1d96cee7
iamBishwa <bishwa1996@gmail.com>
2020-09-03 21:57:44 +0800

    object functions

--- main.cpp
+++ main.cpp
@@ -12,6 +12,12 @@ public:
           pages = aPages;
           price = aPrice;
     }
+    bool goodprice(){
+    if(price >= 120){
+        return true;
+    }
+        return false;
+    }
 };
 
 int main()
@@ -20,7 +26,7 @@ Book book1("Wings of Fire",345,110.50);
 Book book2("Harry Potter",500,126.20);
 
 
-cout<< book1.title;
+cout<< book1.goodprice();
     return 0;
     }
 
